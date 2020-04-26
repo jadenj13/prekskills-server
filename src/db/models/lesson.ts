f7a10cf7433fe38d01c888a52a7e741439286177
jadenj133 <jadenj133@gmail.com>
2020-04-25 22:14:30 -0600

    adds standard to lesson model

--- src/db/models/lesson.ts
+++ src/db/models/lesson.ts
@@ -3,9 +3,14 @@ import * as mongoose from 'mongoose';
 interface ILesson {
   name: string;
   description: string;
+  standard: string;
   questions: Array<{
-    sdImgUrl: string;
-    availableAnswers: Array<{ correct: boolean; imgUrl: string }>;
+    sd: {
+      text: string;
+      imgUrl: string;
+      recordingUrl: string;
+    };
+    answers: Array<{ isCorrect: boolean; imgUrl: string }>;
   }>;
   created: Date;
 }
@@ -15,6 +20,7 @@ export interface LessonDocument extends ILesson, mongoose.Document {}
 const lessonSchema = new mongoose.Schema({
   name: String,
   description: String,
+  standard: String,
   questions: { type: Array, default: [] },
   created: { type: Date, default: Date.now.bind(Date) },
 });
