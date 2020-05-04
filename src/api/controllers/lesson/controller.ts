5e0c3fd68b77d6b756be56f724b382f55753d977
jadenj133 <jadenj133@gmail.com>
2020-05-03 22:12:17 -0600

    adds lesson get all endpoint

--- src/api/controllers/lesson/controller.ts
+++ src/api/controllers/lesson/controller.ts
@@ -3,6 +3,18 @@ import { lessonService } from '../../services';
 import logger from '../../../common/logger';
 
 class Controller {
+  public getAll = async (req: Request, res: Response) => {
+    try {
+      const lessons = await lessonService.getAll();
+
+      return res.json(lessons);
+    } catch (error) {
+      logger.error(error.stack);
+
+      return res.status(500).send(error.message);
+    }
+  };
+
   public create = async (req: Request, res: Response) => {
     try {
       const lesson = await lessonService.create(req.body);
--- src/api/controllers/lesson/router.ts
+++ src/api/controllers/lesson/router.ts
@@ -4,5 +4,6 @@ import allowedRoles from '../../middleware/allowedRoles';
 
 export default express
   .Router()
+  .get('/', allowedRoles(['admin']), controller.getAll)
   .post('/', allowedRoles(['admin']), controller.create)
   .put('/:id', allowedRoles(['admin']), controller.update);
